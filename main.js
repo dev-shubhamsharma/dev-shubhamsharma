d9efafb6ebe10014564bc9427c6e2d6d62616d0b
dev-shubhamsharma <shubhamsharma.gonda@gmail.com>
2020-10-01 21:09:03 +0530

    navigation menu added

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-document.getElementById("menu-btn").addEventListener("click", function(){
-    document.querySelector("#menu-btn .top").style.transform = "rotate(135deg) translateY(-5px)";
-
-    document.querySelector("#menu-btn .bottom").style.transform = "rotate(-135deg) translateY(5px)";
-
-    document.querySelector("#nav").style.left = "0";
+document.getElementById("menu-btn").addEventListener("click",function() {
+    if(document.querySelector("#menu-check").checked == true) {
+        document.querySelector("#menu-btn .top").style.transform = "";
+        document.querySelector("#menu-btn .bottom").style.transform = "";
+        document.querySelector("#nav").style.left = "100%";
+    }
+    else {
+        document.querySelector("#menu-btn .top").style.transform = "rotate(135deg) translateY(-5px)";
+        document.querySelector("#menu-btn .bottom").style.transform = "rotate(-135deg) translateY(5px)";
+        document.querySelector("#nav").style.left = "0";
+    }
 });
